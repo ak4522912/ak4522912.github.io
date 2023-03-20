d1ea3f7809107cd9f203a7d6e4944f984b0af386
ak4522912 <31154526+ak4522912@users.noreply.github.com>
2023-03-20 17:48:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -595,7 +595,7 @@ function getElementByXpath(path) {
       event: "elementSelected",
       selector: t,
       display: e.tagName,
-      dom:{...e},
+      dom:e.target,
       breadcrumb: ue( e ),
       innerHTML: e.innerHTML,
       attributes: ae( e )
