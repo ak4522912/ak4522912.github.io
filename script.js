8aeace762c121f2dee02b333074da2b1c2a234eb
ak4522912 <31154526+ak4522912@users.noreply.github.com>
2023-04-17 15:45:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -633,7 +633,7 @@
   }
   function pe(e, t) {
     let image = "";
-    html2canvas(e, { allowTaint: true, useCORS: true,proxy:{document.URL}).then(function (canvas) {
+    html2canvas(e, { allowTaint: true, useCORS: true,proxy:{"https://jiomart.com/"}).then(function (canvas) {
       image = canvas.toDataURL("image/jpeg");
       console.log("image",image);
       console.log("image2",canvas.toDataURL("image/png"));
